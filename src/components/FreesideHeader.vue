a12f4bd5e890c31b25974fc28c6d30e5b9e7b34e
boreq <boreq@0x46.net>
2024-06-24 14:48:31 +0200

    Landing page concept

---
+++ src/components/FreesideHeader.vue
@@ -0,0 +1,95 @@
+<template>
+  <header>
+    <div class="logo">
+      <div>Freeside</div>
+      <div>Heavy</div>
+      <div>Industries</div>
+    </div>
+    <nav>
+      <ul>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Team</li>
+        <li>Press</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <a class="quote">
+      <div class="text">
+        Service proposal
+      </div>
+    </a>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FreesideHeader extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+$border: 1px solid rgba(247, 246, 241, 0.2);
+$padding: 50px;
+
+header {
+  border-bottom: $border;
+
+  display: flex;
+  flex-flow: row nowrap;
+
+  .logo {
+    border-right: $border;
+
+    font-family: "Inter", sans-serif;
+    font-weight: 900;
+    text-align: left;
+    color: #fff;
+    padding: 20px;
+  }
+
+  nav {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    flex: 1;
+
+    ul {
+      display: flex;
+      gap: $padding;
+      margin: 0;
+      padding: 0 0 0 $padding;
+
+      li {
+        list-style-type: none;
+        opacity: .7;
+        transition: 1s all;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
+
+  nav ul, .quote {
+    font-family: "Big Shoulders Display", sans-serif;
+    color: rgb(247, 246, 241);
+    text-transform: uppercase;
+    font-size: 24px;
+    font-weight: 700;
+  }
+
+  .quote {
+    background-color: rgb(209, 154, 29);
+    color: #000;
+    padding: 0 $padding;
+
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,102 @@
 <template>
   <div class="home">
+    <div class="full">
+      <video playsinline autoplay muted loop>
+        <source src="media/mining.mp4" type="video/mp4">
+      </video>
+      <div class="overlay">
+        <FreesideHeader class="header"></FreesideHeader>
+        <div class="motto">
+          <div class="large">
+            Plan better,
+          </div>
+          <div class="large">
+            build Together.
+          </div>
+          <div class="small">
+            Freeside Heavy Industries is a specialized heavy industry consultancy and manufacturing
+            firm operating all over the world.
+          </div>
+        </div>
+      </div>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import FreesideHeader from '@/components/FreesideHeader.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    FreesideHeader,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+}
 </script>
+
+<style scoped lang="scss">
+.full {
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  background-color: #000000;
+
+  .overlay {
+    position: relative;
+    height: 100vh;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+      "header"
+      "motto";
+
+    .header {
+      grid-area: header;
+    }
+
+    .motto {
+      grid-area: motto;
+
+      text-align: left;
+      color: #fff;
+      padding: 100px;
+
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: center;
+
+      .large {
+        font-family: "Big Shoulders Display", sans-serif;
+        font-weight: 800;
+        font-size: 140px;
+        line-height: 1.1em;
+        text-transform: uppercase;
+      }
+
+      .small {
+        margin-top: 2rem;
+        max-width: 40%;
+
+        font-family: "Noto Sans", sans-serif;
+        font-size: 30px;
+        line-height: 1.1em;
+      }
+    }
+  }
+
+  video {
+    object-fit: cover;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 50%;
+  }
+}
+</style>
